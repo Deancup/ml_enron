caf76f79cc47dffb55b39d4bb8fb45d9bfd31b68
Dean <gugecup@gmail.com>
2017-07-10 09:23:57 +0800

    featrues_lists_dones

---
+++ poi_id.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python
+
+import sys
+import pickle
+sys.path.append("../tools/")
+
+from feature_format import featureFormat, targetFeatureSplit
+from tester import dump_classifier_and_data
+from sklearn.feature_selection import SelectKBest
+
+### get integral features_list of dataset
+features_list_total=[]
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+    for item in data_dict:
+        for elements in data_dict[item]:
+            features_list_total.append(elements)
+        break
+
+features_list_total=['poi','salary', 'to_messages', 'deferral_payments', 'total_payments',
+                      'exercised_stock_options', 'bonus', 'restricted_stock',
+                      'shared_receipt_with_poi', 'restricted_stock_deferred',
+                      'total_stock_value', 'expenses', 'loan_advances',
+                      'from_messages', 'other', 'from_this_person_to_poi',
+                      'director_fees', 'deferred_income', 'long_term_incentive',
+                      'from_poi_to_this_person']
+my_dataset=data_dict
+print len(my_dataset)
+data = featureFormat(my_dataset, features_list_total, sort_keys = True)
+labels_select, features_select = targetFeatureSplit(data)
+#print labels_select,features_select
+feature_selector=SelectKBest(k=7)
+feature_selector.fit(features_select,labels_select)
+print feature_selector.get_support()
+features_select_transformed=feature_selector.transform(features_select)
+### Task 1: Select what features you'll use.
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+
+def get_feature_list(features_list_total):
+    features_list = []
+    for item,boolean in zip(features_list_total,feature_selector.get_support()):
+        if boolean==True:
+            features_list.append(item)
+    return features_list
+features_list=get_feature_list(features_list_total)
+# You will need to use more features
+### Load the dictionary containing the dataset and
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
+
+
+### Task 2: Remove outliers
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+my_dataset = data_dict
+
+### Extract features and labels from dataset for local testing
+data = featureFormat(my_dataset, features_list, sort_keys = True)
+labels, features = targetFeatureSplit(data)
+
+### Task 4: Try a varity of classifiers
+### Please name your classifier clf for easy export below.
+### Note that if you want to do PCA or other multi-stage operations,
+### you'll need to use Pipelines. For more info:
+### http://scikit-learn.org/stable/modules/pipeline.html
+
+# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+### using our testing script. Check the tester.py script in the final project
+### folder for details on the evaluation method, especially the test_classifier
+### function. Because of the small size of the dataset, the script uses
+### stratified shuffle split cross validation. For more info: 
+### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
+
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### check your results. You do not need to change anything below, but make sure
+### that the version of poi_id.py that you submit can be run on its own and
+### generates the necessary .pkl files for validating your results.
+
+dump_classifier_and_data(clf, my_dataset, features_list)
